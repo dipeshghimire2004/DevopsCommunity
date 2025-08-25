25025f2eb6f0ffdfcc96c3cbfcaec881c393f447
dipeshghimire2004 <ghimiredipesh2004@gmail.com>
2025-08-25 07:42:22 +0545

    add export on next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enable static export
+  trailingSlash: true, //# URLs end with /
+  images: {
+    unoptimized: true, //# Required for static export
+  },
+  // Optional: Custom export path
+  // distDir: 'build',        # Changes .next to build
+  // assetPrefix: '/my-app/', # If deploying to subdirectory
 };
 
 export default nextConfig;
